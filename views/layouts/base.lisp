3d3f4dfb0158e80e16ae3af3d246318440add236
James <jamesballsemail@gmail.com>
2018-03-20 09:46:30 +0000

    Cleaned Up Base Layout

--- views/layouts/base.lisp
+++ views/layouts/base.lisp
@@ -3,52 +3,31 @@
 (deflayout base (data :top-level t)
   (:html
     (:head
-      (:title (str (conc (getf data :title) " | Wookie")))
+      (:title (str (conc (getf data :title) " | James Ball")))
       (:link :rel "stylesheet" :href "/css/template.css?2")
-      (:link :rel "stylesheet" :href "/css/modal.css")
       (:link :rel "shortcut icon" :href "/favicon.png" :type "image/png")
-      (:script :src "/js/mootools-1.4.1.js")
-      (:script :src "/js/modal.js")
       (:script :src "/js/wookie.js")
       ;; syntax highlighting
       (:link :rel "stylesheet" :href "/js/highlight-lisp/themes/wookie.css" :id "hl-style")
       (:script :src "/js/highlight-lisp/highlight-lisp.js"))
     (:body :class (getf data :body-class)
-      (:div :id "wookie" (:a :href "/" "&nbsp;"))
       (:div :id "container"
         (:header
           (:div :class "inner"
-            (:h1 (:a :href "/" "Wookie"))
+            (:h1 (:a :href "/" "james-ball.co.uk"))
             (:nav
               (:ul
+                (:li (:a :href "/portfolio" "Portfolio"))
                 (:li (:a :href "/docs" "Docs"))
-                (:li (:a :href "/faq" "FAQ"))
-                ;(:li (:a :href "/apps" "Apps"))
+                (:li (:a :href "/blog" "Blog"))
                 (:li (:a :href "/about" "About"))
-                (:li (:a :href "https://github.com/orthecreedence/wookie" "Github"))
-                ;(:li (:a :href "/guide" "Guide"))
+                (:li (:a :href "/contact" "Contact"))
+                (:li (:a :href "https://gituhb.com/84115/84115" "Github"))
                 ))))
         (str (getf data :content))
         
         (:footer
           (:ul
-            (:li (:a :href "/" "Wookie home"))
-            (:li (:a :href "https://github.com/orthecreedence/wookie" "github"))
-            (:li "Powered by Wookie"))
-          (:p "MIT Licensed | &copy;" (:a :href "http://lyonbros.com" "Lyon Bros. Enterprises, LLC"))))
-      (:script
-        "
-  var _paq = _paq || [];
-  _paq.push([\"trackPageView\"]);
-  _paq.push([\"enableLinkTracking\"]);
-
-  (function() {
-    var u=((\"https:\" == document.location.protocol) ? \"https\" : \"http\") + \"://stats.killtheradio.net/\";
-    _paq.push([\"setTrackerUrl\", u+\"piwik.php\"]);
-    _paq.push([\"setSiteId\", \"7\"]);
-    var d=document, g=d.createElement(\"script\"), s=d.getElementsByTagName(\"script\")[0]; g.type=\"text/javascript\";
-    g.defer=true; g.async=true; g.src=u+\"piwik.js\"; s.parentNode.insertBefore(g,s);
-  })();
-        "))))
-
-
+            (:li (:a :href "/sitemap" "Sitemap"))
+            (:li (:a :href "https://gituhb.com/84115/84115" "Github"))
+          (:p "MIT Licensed | &copy;")))))))
