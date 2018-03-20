7123e137672eb5d313f0f71e59e1a6cc2365424e
James <jamesballsemail@gmail.com>
2018-03-20 11:33:01 +0000

    Updated main.js

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+var init = function init(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    }
+    else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+init(function() {
+    if (typeof HighlightLisp !== "undefined") {
+        HighlightLisp.highlight_auto();
+    }
+
+    console.log("Hello World");
+});
